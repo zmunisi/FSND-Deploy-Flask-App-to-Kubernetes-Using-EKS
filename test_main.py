aa4dcad8e3ae0b9dca7bf3c031a7e89cfb7474b3
zmunisi <zmunisi.ext@gmail.com>
2021-06-12 00:09:23 +0200

    Intentionally assert a False test to test the build

--- test_main.py
+++ test_main.py
@@ -24,8 +24,9 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 200
-    assert response.json == 'Healthy'
+    assert False
+    # assert response.status_code == 200
+    # assert response.json == 'Healthy'
 
 
 def test_auth(client):
