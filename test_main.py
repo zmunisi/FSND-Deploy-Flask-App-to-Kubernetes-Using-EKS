e8c9aebe207ff1d1277f03e39d2a5827a9991f91
zmunisi <zmunisi.ext@gmail.com>
2021-06-12 00:13:59 +0200

    Revert False Assert Test

--- test_main.py
+++ test_main.py
@@ -24,9 +24,8 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert False
-    # assert response.status_code == 200
-    # assert response.json == 'Healthy'
+    assert response.status_code == 200
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
